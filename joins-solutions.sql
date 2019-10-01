17cd18ddf4990559b70158e700f450b54da0f9bf
Naomi Price <naomi.price71@gmail.com>
2019-10-01 11:32:20 -0500

    Q-2 get all orders and their line items

--- joins-solutions.sql
+++ joins-solutions.sql
@@ -5,3 +5,18 @@ JOIN "addresses"
 	ON "customers".id = "addresses".customer_id;
 
 -- 2. Get all orders and their line items (orders, quantity and product)
+SELECT *
+FROM "orders"
+JOIN "line_items"
+	ON "orders".id = "line_items".order_id
+JOIN "products" 
+	ON "products".id = "line_items".product_id;
+--OR
+SELECT "orders".id, "orders".order_date, "line_items".quantity, "products".description, "products".unit_price
+FROM "orders"
+JOIN "line_items"
+	ON "orders".id = "line_items".order_id
+JOIN "products" 
+	ON "products".id = "line_items".product_id;
+
+-- 3. Which warehouses have cheetos?
